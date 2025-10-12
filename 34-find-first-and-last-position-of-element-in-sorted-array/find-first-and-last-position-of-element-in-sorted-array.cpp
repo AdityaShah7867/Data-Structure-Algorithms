a089c3477fe7761214500aabaf88036ab6ec780a
ADITYA NIKUNJ SHAH <192105adityashah@gmail.com>
2025-10-12 23:31:57 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (17.61%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int a = -1;
+        int b = -1;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == target) {
+                if (a == -1) {
+                    a = i;
+                }
+                b = i;
+            }
+        }
+        return {a, b};
+    }
+};
