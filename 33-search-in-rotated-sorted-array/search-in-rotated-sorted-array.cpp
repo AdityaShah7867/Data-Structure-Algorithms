ed530d24d1cd3c81bfd6dd099128c2b672b4459d
ADITYA NIKUNJ SHAH <192105adityashah@gmail.com>
2026-03-24 23:44:05 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (67.09%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -3,29 +3,28 @@ public:
     int search(vector<int>& nums, int target) {
         int st = 0, end=nums.size()-1;
 
-        while(st<=end){
+        while(st<= end){
             int mid = st + (end-st)/2;
-            if (nums[mid] == target){
+
+            if (nums[mid] == target ){
                 return mid;
             }
 
-            if(nums[st] <= nums[mid]){
-                //left sorted
+            if (nums[st] <= nums[mid]){
                 if(nums[st] <= target && target <= nums[mid]){
                     end = mid-1;
                 }else{
                     st = mid+1;
                 }
             }else{
-                //right sorted
                 if(nums[mid] <= target && target <= nums[end]){
-                    st = st +1;
+                    st = mid+1;
                 }else{
-                    end = end-1;
+                    end = mid-1;
                 }
-
             }
         }
+
         return -1;
         
     }
